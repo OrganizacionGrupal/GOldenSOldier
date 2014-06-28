9244bf52e95ad0569b26fc62a4e9036aa04e9191
gabrielhottman <gabrielhottman@hotmail.com>
2014-06-28 03:56:36 -0500

    Casí Terminando el Primer ejercicio

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicios;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Ejercicio1 
+{
+    
+    
+
+    
+    
+    public int sumaElementos(int x, int y)
+    {
+        int mayor;
+        if (x<y) 
+        {
+            mayor=y;
+        }
+        else if (x>y)
+        {
+            mayor=x;
+        }
+        else
+        {
+            mayor=x;
+        }
+        int vectorA[]=new int [mayor];
+        
+        int con=7;
+        int con2=6;
+        for(int i=0;i<mayor;i++)
+        {
+            
+            vectorA[i]=con;
+            i=i+1;
+            vectorA[i]=con2;
+            con=con+1;
+            con2=con2-2;
+        }
+        int suma;
+        if (x<=0||x>255||y<=0||y>255)
+        {
+            return -1;
+        }
+        int posicionX=x-1;
+        int posicionY=y-1;
+        
+        suma = vectorA[posicionX]+vectorA[posicionY];
+             
+        
+            
+         return suma;   
+    }
+        
+}
--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -6,17 +6,34 @@
 
 package ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gabriel
  */
-public class Ejercicios {
+public class Ejercicios 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Scanner entrada = new Scanner(System.in);
         // TODO code application logic here
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        System.out.print("Ingrese el valor de X: ");
+        int A=entrada.nextInt();
+        System.out.print("Ingrese el valor de Y: ");
+        int B=entrada.nextInt();
+        
+        System.out.println("La suma de sus elementos es: "+ejercicio1.sumaElementos(A, B));
+        
+        
+        
+  
+        
     }
     
 }
