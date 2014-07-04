b1e93316067e40693f73c209193346378ca407b7
gabrielhottman <gabrielhottman@hotmail.com>
2014-07-04 04:04:50 -0500

    aciendo modificaciones

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -12,55 +12,54 @@ package ejercicios;
  */
 public class Ejercicio1 
 {
-    
-    
 
-    
-    
     public int sumaElementos(int x, int y)
     {
         /*Aquí vemos cual es el numero mayor para en base a eso calcular
           la máxima posición de la serie*/
         
-        int mayor;
-        if (x<y) 
-        {
-            mayor=y;
-        }
-        else if (x>y)
+        int mayor,menor;
+        if (x>y) 
         {
             mayor=x;
+            menor=y;
+        }
+        else if (y>x)
+        {
+            mayor=y;
+            menor=x;
         }
         else
         {
             mayor=x;
+            menor=y;
         }
         int vectorA[]=new int [mayor];
         
-        int con=7;
-        int con2=6;
-        for(int i=0;i<mayor;i++)
+        int se=7;
+        int se2=6;
+        
+        
+        for (int i = 0; i <= mayor; i++) 
         {
-            
-            vectorA[i]=con;
-            i=i+1;
-            vectorA[i]=con2;
-            con=con+1;
-            con2=con2-2;
+            vectorA[i]=se;
+            i=+1;
+            vectorA[i]=se2;
+            se=+2;
+            se2=-2;
         }
+               
+        
         int suma;
         if (x<=0||x>255||y<=0||y>255)
         {
             return -1;
         }
-        int posicionX=x-1;
-        int posicionY=y-1;
+
         
-        suma = vectorA[posicionX]+vectorA[posicionY];
-             
-        
-            
-         return suma;   
+        suma = vectorA[x]+vectorA[y];
+         
+        return suma;   
     }
         
 }
